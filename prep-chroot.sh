9029f5c6f7e94d9da1ff6f13ff87c6dcd4e2f45b
Jon Sanchez <jon@jon-s.com>
2022-06-07 19:43:12 -0700

    Mount special filesystems in chroot

--- prep-chroot.sh
+++ prep-chroot.sh
@@ -25,23 +25,65 @@ prepchroot() {
 #!/bin/bash
 echo "builduser:x:1000:1000:builduser:/build:/bin/bash" >> /etc/passwd
 echo "builduser:x:1000:" >> /etc/group
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_CA.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 EOF
 	chmod +x "$TMPDIR/insidechroot.sh"
 }
 
+create() {
+	mkdir -p "$TMPDIR/var/lib/pacman"
+	sudo pacman -r "$TMPDIR" -Sy base-devel
+	sudo mount proc "$TMPDIR/proc" -t proc -o nosuid,noexec,nodev
+	sudo mount sys "$TMPDIR/sys" -t sysfs -o nosuid,noexec,nodev,ro
+	sudo mount udev "$TMPDIR/dev" -t devtmpfs -o mode=0755,nosuid
+	sudo mount devpts "$TMPDIR/dev/pts" -t devpts -o mode=0620,gid=5,nosuid,noexec
+
+	mkdir "$TMPDIR/build"
+	cp ~/.makepkg.conf "$TMPDIR/build/.makepkg.conf"
+
+	prepchroot
+	sudo chroot "$TMPDIR" "/insidechroot.sh"
+
+	echo "Build chroot prepared"
+	echo "Invoke 'pacman -r $TMPDIR --cache-dir $TMPDIR/var/cache/pacman/pkg'"
+	echo "  to install additional dependancies into the chroot"
+	echo ""
+	echo "Invoke 'sudo -u builduser makepkg' while chrooted to run makepkg"
+	echo ""
+	echo "To remove chroot, launch script with clean action"
+}
+
+clean() {
+	sudo umount "$TMPDIR/dev/pts"
+	sudo umount "$TMPDIR/dev"
+	sudo umount "$TMPDIR/sys"
+	sudo umount "$TMPDIR/proc"
+}
+
+usage() {
+	echo "Usage: $0 create|clean|help"
+	echo ""
+	echo "create: creates a build chroot"
+	echo "clean: cleans the system mounts for a build chroot"
+	echo "help: shows this message"
+}
+
 checkroot
 
-mkdir -p "$TMPDIR/var/lib/pacman"
-sudo pacman -r "$TMPDIR" -Sy base-devel
-
-mkdir "$TMPDIR/build"
-cp ~/.makepkg.conf "$TMPDIR/build/.makepkg.conf"
-
-prepchroot
-sudo chroot "$TMPDIR" "/insidechroot.sh"
-
-echo "Build chroot prepared"
-echo "Invoke 'pacman -r $TMPDIR --cache-dir $TMPDIR/var/cache/pacman/pkg'"
-echo "  to install additional dependancies into the chroot"
-echo ""
-echo "Invoke 'sudo -u builduser makepkg' while chrooted to run makepkg"
+case $1 in
+create)
+	create
+	;;
+clean)
+	clean
+	;;
+help)
+	usage
+	;;
+*)
+	usage
+	;;
+esac
